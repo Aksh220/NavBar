339a1d02005d1b4a7a3b6a02cf57bd780388bb26
Aksh2209 <akshayap672@gmail.com>
2020-06-25 20:34:14 +0530

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -10,11 +11,16 @@ import { HeaderComponent } from './global/header.component';
 import{ DashboardComponent } from './dashboard/dashboard.component';
 import {MatIconModule} from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
 import { appRoutes } from './app.route';
 
 @NgModule({
   imports: [
-    BrowserModule, FormsModule , MatIconModule, MatSidenavModule,
+    BrowserModule, FormsModule , BrowserAnimationsModule, MatIconModule, MatSidenavModule,MatTabsModule,MatButtonModule, MatCheckboxModule, MatToolbarModule, MatListModule,
     RouterModule.forRoot (appRoutes)
   ],
   declarations: [ 
