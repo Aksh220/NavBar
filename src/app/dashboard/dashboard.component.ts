6593ad65fb96bd6d9d657bf9a86856c796ff176f
Aksh2209 <akshayap672@gmail.com>
2020-06-26 12:24:52 +0530

    2 files modified, 1 added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'dashboard-section',
-  template:`<p> Torry Harris Business Solution </p>`
+  template:`<p> Torry Harris Business Solution </p>`,
+  styleUrls :['dashboard.component.css']
 })
 export class DashboardComponent { }
